3864fc8f5633d7ea3180e3eba058775d01f3a6c9
ABBush2001 <qdf438@my.utsa.edu>
2022-10-22 11:54:47 -0500

    fade in and out

--- Assets/Scripts/TriggerChecker.cs
+++ Assets/Scripts/TriggerChecker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerChecker : MonoBehaviour
 {
@@ -9,26 +10,39 @@ public class TriggerChecker : MonoBehaviour
         if(collision.gameObject.tag == "t1")
         {
             Debug.Log("trigger 1");
+            GameObject.Find("Main Camera").GetComponent<CameraFadeOut>().fadeOut = true;
+            StartCoroutine(waitForFade(collision));
         }
 
         if (collision.gameObject.tag == "t2")
         {
             Debug.Log("trigger 2");
+            GameObject.Find("Main Camera").GetComponent<CameraFadeOut>().fadeOut = true;
         }
 
         if (collision.gameObject.tag == "t3")
         {
             Debug.Log("trigger 3");
+            GameObject.Find("Main Camera").GetComponent<CameraFadeOut>().fadeOut = true;
         }
 
         if (collision.gameObject.tag == "t4")
         {
             Debug.Log("trigger 4");
+            GameObject.Find("Main Camera").GetComponent<CameraFadeOut>().fadeOut = true;
         }
 
         if (collision.gameObject.tag == "t5")
         {
             Debug.Log("trigger 5");
+            GameObject.Find("Main Camera").GetComponent<CameraFadeOut>().fadeOut = true;
         }
+    }
+
+    IEnumerator waitForFade(Collider2D collision)
+    {
+        yield return new WaitForSeconds(5);
+        GameObject.Find("Submarine").transform.SetPositionAndRotation(new Vector3(-6.8829f, collision.transform.position.y - 10, 0), new Quaternion());
+        GameObject.Find("SceneManager").GetComponent<LoadNextScene>().LoadScene(2);
     }
 }
